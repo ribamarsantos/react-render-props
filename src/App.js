c9e889f3084457cdf7c0377ebf0c154ee9e54c5d
Ribamar Marcos <ribamaar@gmail.com>
2018-09-14 15:15:49 -0500

    Creating mouse component - part 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// The <Mouse> component encapsulates the behavior we need...
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
+
+        {/* ...but how do we render something other than a <p>? */}
+        <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,9 +43,9 @@ class App extends Component {
 
   render() {
     return (
-      <div style={{ height: '100%', backgroundColor: 'green' }} onMouseMove={this.handleMouseMove}>
+      <div>
         <h1>Move the mouse around!</h1>
-        <p>The current mouse position is ({this.state.x}, {this.state.y})</p>
+        <Mouse />
       </div>
     );
   }
